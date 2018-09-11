b4650c1d4c9072436c6f232bf5cad1a1fdda163f
Marcos <marcosl172@hotmail.com>
2018-09-11 00:17:40 -0300

    JPA en Escuela Factory
    Aplicacion de JPA en la clase EscuelaFactory

--- src/org/aplicacionAndroid/Escuela/Escuelafactory.java
+++ src/org/aplicacionAndroid/Escuela/Escuelafactory.java
@@ -7,7 +7,7 @@ import org.aplicacionAndroid.Profesores.Profesores;
 public class Escuelafactory
 {
 	
-
+	@Id @Column(length=20) @Requiered
 	puclic String escuela;
 
 	public Profesores profesores;
